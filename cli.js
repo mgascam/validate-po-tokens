3b7de358124275f9af1971aae7701d0a03991e8b
Miguel Gasca <miguelgasca@gmail.com>
2019-11-02 00:37:49 +0100

    add cli.js

--- cli.js
+++ cli.js
@@ -1,13 +1,19 @@
+#!/usr/bin/env node
+
 const { getEntriesFromFile } = require('./lib/entries');
 const { searchTokenInMessages, validateTokens } = require('./lib/tokens');
 
-const file = './lib/sample/locale/pt/LC_MESSAGES/django.po';
+// const file = './lib/sample/locale/es/LC_MESSAGES/django.po';
 const regExps = [/<\[.*?\]>/gm, /\{[^{}]+\}/gm, /\$\{.*?\}/gm, /{{[^{]+}}/gm, /\%\(.*?\)/gm, /\[.*?\]/gm];
+
+// Grab args
+const [,, ...args] = process.argv;
+const file = args[0];
 
 getEntriesFromFile(file).then((data) => {
   regExps.forEach(regexp => {
     console.log(` REGEXP ${regexp}`);
     const findTokens = searchTokenInMessages(regexp, data);
-    console.log(validateTokens(findTokens));
+    console.dir(validateTokens(findTokens));
   });
 });
